2e7e4baafab2109aabed54c29b78bed6ac4ececc
julietilleul <julietilleul@gmail.com>
2020-10-21 17:36:51 +0300

    refactored chartIQ SDK + demo implementation. separated view and BL of the chartIQ functionality

--- demo/src/main/java/com/chartiq/demo/ui/chart/ChartFragment.kt
+++ demo/src/main/java/com/chartiq/demo/ui/chart/ChartFragment.kt
@@ -10,10 +10,12 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.chartiq.demo.ApplicationPrefs
 import com.chartiq.demo.BuildConfig
+import com.chartiq.demo.ChartIQApplication
 import com.chartiq.demo.R
 import com.chartiq.demo.databinding.FragmentChartBinding
 import com.chartiq.demo.network.ChartIQNetworkManager
 import com.chartiq.demo.ui.chart.interval.model.TimeUnit
+import com.chartiq.sdk.ChartIQHandler
 import com.chartiq.sdk.DataSource
 import com.chartiq.sdk.DataSourceCallback
 import com.chartiq.sdk.model.DataMethod
@@ -22,6 +24,9 @@ import com.chartiq.sdk.model.QuoteFeedParams
 
 class ChartFragment : Fragment() {
 
+    private val chartIQHandler: ChartIQHandler by lazy {
+        (requireActivity().application as ChartIQApplication).chartIQHandler
+    }
     private lateinit var binding: FragmentChartBinding
     private val chartViewModel: ChartViewModel by viewModels(factoryProducer = {
         ChartViewModel.ChartViewModelFactory(ChartIQNetworkManager())
@@ -42,37 +47,38 @@ class ChartFragment : Fragment() {
     }
 
     private fun setupViews() {
-        with(binding) {
-            val symbol = prefs.getChartSymbol().value
+        val symbol = prefs.getChartSymbol().value
+        chartIQHandler.apply {
+            chartIQView = binding.chartIqView
+            start(BuildConfig.DEFAULT_CHART_URL) {
+                setDataMethod(DataMethod.PULL, symbol)
+                setSymbol(symbol)
+                setDataSource(object : DataSource {
+                    override fun pullInitialData(
+                        params: QuoteFeedParams,
+                        callback: DataSourceCallback
+                    ) {
+                        loadChartData(params, callback)
+                    }
 
-            with(webview) {
-                start(BuildConfig.DEFAULT_CHART_URL) {
-                    setDataMethod(DataMethod.PULL, symbol)
-                    setSymbol(symbol)
-                    setDataSource(object : DataSource {
-                        override fun pullInitialData(
-                            params: QuoteFeedParams,
-                            callback: DataSourceCallback
-                        ) {
-                            loadChartData(params, callback)
-                        }
+                    override fun pullUpdateData(
+                        params: QuoteFeedParams,
+                        callback: DataSourceCallback
+                    ) {
+                        loadChartData(params, callback)
+                    }
 
-                        override fun pullUpdateData(
-                            params: QuoteFeedParams,
-                            callback: DataSourceCallback
-                        ) {
-                            loadChartData(params, callback)
-                        }
-
-                        override fun pullPaginationData(
-                            params: QuoteFeedParams,
-                            callback: DataSourceCallback
-                        ) {
-                            loadChartData(params, callback)
-                        }
-                    })
-                }
+                    override fun pullPaginationData(
+                        params: QuoteFeedParams,
+                        callback: DataSourceCallback
+                    ) {
+                        loadChartData(params, callback)
+                    }
+                })
             }
+        }
+
+        with(binding) {
             symbolButton.apply {
                 setOnClickListener {
                     findNavController().navigate(R.id.action_mainFragment_to_searchSymbolFragment)
@@ -99,19 +105,19 @@ class ChartFragment : Fragment() {
                     findNavController().navigate(R.id.action_mainFragment_to_drawingToolFragment)
                 }
             }
-        }
 
-        chartViewModel.resultLiveData.observe(viewLifecycleOwner) { chartData ->
-            binding.webview.post {
-                chartData.callback.execute(chartData.data)
+            chartViewModel.resultLiveData.observe(viewLifecycleOwner) { chartData ->
+                binding.chartIqView.post {
+                    chartData.callback.execute(chartData.data)
+                }
             }
-        }
-        chartViewModel.errorLiveData.observe(viewLifecycleOwner) {
-            Toast.makeText(
-                requireContext(),
-                getString(R.string.warning_something_went_wrong),
-                Toast.LENGTH_SHORT
-            ).show()
+            chartViewModel.errorLiveData.observe(viewLifecycleOwner) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.warning_something_went_wrong),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
