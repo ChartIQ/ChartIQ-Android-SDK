41dcab147a3592ed9fc87e3b314767c37c5366bb
Yuliia Lypovetska <yuliia.lypovetska@codeit.pro>
2020-11-02 18:54:08 +0200

    add webview debugger

--- demo/src/main/java/com/chartiq/demo/ChartIQApplication.kt
+++ demo/src/main/java/com/chartiq/demo/ChartIQApplication.kt
@@ -1,11 +1,20 @@
 package com.chartiq.demo
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.webkit.WebView
 import com.chartiq.sdk.ChartIQHandler
 
 class ChartIQApplication : Application() {
 
     val chartIQHandler: ChartIQHandler by lazy {
         ChartIQHandler(BuildConfig.DEFAULT_CHART_URL, this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
     }
 }
