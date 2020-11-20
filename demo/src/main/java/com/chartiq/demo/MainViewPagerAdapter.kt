8154dc2a0bf937cfdb3af163b764cd73ae2f8f3f
Dmytro Shuba <dmytro.shuba@codeit.pro>
2020-11-20 17:27:37 +0200

    Fix MainViewPagerAdapter navigation tabs

--- demo/src/main/java/com/chartiq/demo/MainViewPagerAdapter.kt
+++ demo/src/main/java/com/chartiq/demo/MainViewPagerAdapter.kt
@@ -9,24 +9,20 @@ import com.chartiq.demo.ui.study.StudyFragment
 class MainViewPagerAdapter(fragment: Fragment) :
     FragmentStateAdapter(fragment) {
 
-    override fun getItemCount(): Int = listOf(
-        FRAGMENT_CHART,
-        FRAGMENT_STUDIES,
-        FRAGMENT_SETTINGS
-    ).size
+    override fun getItemCount(): Int = MainNavigation.values().size
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            FRAGMENT_CHART -> ChartFragment()
-            FRAGMENT_STUDIES -> StudyFragment()
-            FRAGMENT_SETTINGS -> SettingsFragment()
+            MainNavigation.FRAGMENT_CHART.value -> ChartFragment()
+            MainNavigation.FRAGMENT_STUDIES.value -> StudyFragment()
+            MainNavigation.FRAGMENT_SETTINGS.value -> SettingsFragment()
             else -> throw IllegalStateException()
         }
     }
 
-    companion object {
-        const val FRAGMENT_CHART = 0
-        const val FRAGMENT_STUDIES = 1
-        const val FRAGMENT_SETTINGS = 2
+    enum class MainNavigation(val value: Int) {
+        FRAGMENT_CHART(0),
+        FRAGMENT_STUDIES(1),
+        FRAGMENT_SETTINGS(2)
     }
 }
