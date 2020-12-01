a1c10b34de13346dc1dace8730fe34ede6756eeb
Dmytro Shuba <dmytro.shuba@codeit.pro>
2020-12-01 19:24:08 +0200

    Provide minor code improvements to SDK

--- chartiq/src/main/java/com/chartiq/sdk/ChartIQ.kt
+++ chartiq/src/main/java/com/chartiq/sdk/ChartIQ.kt
@@ -5,7 +5,7 @@ import android.view.View
 import com.chartiq.sdk.model.*
 import java.util.*
 
-interface ChartIQ: ChartIQDrawingTool {
+interface ChartIQ: ChartIQDrawingTool, ChartIQStudy {
 
     val chartView: View
 
@@ -23,33 +23,15 @@ interface ChartIQ: ChartIQDrawingTool {
 
     fun setPeriodicity(period: Int, interval: String, timeUnit: String)
 
-    fun getStudyList(callback: OnReturnCallback<List<Study>>)
-
-    override fun getActiveStudies(callback: OnReturnCallback<List<Study>>)
-
     fun setAggregationType(aggregationType: AggregationType)
 
     fun setChartType(chartType: ChartType)
 
     fun setChartScale(scale: ChartScale)
 
-    override fun removeStudy(study: Study)
-
-    override fun addStudy(study: Study, forClone: Boolean)
-
-    override fun setStudyParameter(study: Study, parameter: StudyParameterModel)
-
-    override fun setStudyParameters(study: Study, parameters: List<StudyParameterModel>)
-
     fun setOHLCParameters(talkbackFields: HashMap<String, Boolean>)
 
     fun getHUDDetails(callback: OnReturnCallback<CrosshairHUD>)
-
-    override fun getStudyParameters(
-        study: Study,
-        type: StudyParameterType,
-        callback: OnReturnCallback<List<StudyParameter>>
-    )
 
     companion object {
         fun getInstance(url: String, context: Context): ChartIQ {
