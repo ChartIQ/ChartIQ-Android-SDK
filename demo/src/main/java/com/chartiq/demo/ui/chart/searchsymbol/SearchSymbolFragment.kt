5d900599417837ead5a864a2132f50291af66a32
dmytro.shuba@codeit.pro <dmytro.shuba@codeit.pro>
2020-12-01 09:39:21 +0200

    Provide minor fixes

--- demo/src/main/java/com/chartiq/demo/ui/chart/searchsymbol/SearchSymbolFragment.kt
+++ demo/src/main/java/com/chartiq/demo/ui/chart/searchsymbol/SearchSymbolFragment.kt
@@ -14,6 +14,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.SearchView.SearchAutoComplete
+import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -46,7 +47,7 @@ class SearchSymbolFragment : Fragment(), OnSearchResultClickListener, VoiceQuery
                 symbolNotFoundPlaceholder.root.isVisible = false
                 typeToSearchPlaceholder.root.isVisible = false
                 searchSymbolProgressBar.isVisible = true
-                queryResultsRecyclerView.visibility = View.INVISIBLE
+                queryResultsRecyclerView.isInvisible = true
             }
         }
 
