8cbddd3dc477a78ced21aef409cd0e833ea0191b
dmytro.shuba@codeit.pro <dmytro.shuba@codeit.pro>
2020-12-02 05:27:31 +0200

    Add filters to search symbol; Refactor SearchResultAdapter.kt; Refactor SearchSymbolViewModel.kt; Provide code improvements and code fixes

--- demo/src/main/java/com/chartiq/demo/ui/chart/searchsymbol/SearchSymbolFragment.kt
+++ demo/src/main/java/com/chartiq/demo/ui/chart/searchsymbol/SearchSymbolFragment.kt
@@ -1,6 +1,5 @@
 package com.chartiq.demo.ui.chart.searchsymbol
 
-import android.app.Activity
 import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
@@ -13,7 +12,6 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.SearchView.SearchAutoComplete
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -25,49 +23,59 @@ import com.chartiq.demo.network.ChartIQNetworkManager
 import com.chartiq.demo.ui.LineItemDecoration
 import com.chartiq.demo.ui.chart.searchsymbol.list.OnSearchResultClickListener
 import com.chartiq.demo.ui.chart.searchsymbol.list.SearchResultAdapter
-import com.chartiq.demo.ui.chart.searchsymbol.list.SearchResultItem
 import com.chartiq.demo.util.hideKeyboard
+import com.google.android.material.tabs.TabLayout
 
 import androidx.appcompat.R.id as appCompat
 
 
-class SearchSymbolFragment : Fragment(), OnSearchResultClickListener, VoiceQueryReceiver {
+class SearchSymbolFragment : Fragment(), VoiceQueryReceiver {
 
     private lateinit var binding: FragmentSearchSymbolBinding
     private val viewModel: SearchSymbolViewModel by viewModels(factoryProducer = {
-        SearchSymbolViewModel.SearchViewModelFactory(ChartIQNetworkManager())
+        SearchSymbolViewModel.SearchViewModelFactory(
+            ChartIQNetworkManager(),
+            ApplicationPrefs.Default(requireContext())
+        )
     })
+    private val searchAdapter = SearchResultAdapter()
 
     // Since the app reuses native Google voice recognition the voice query is sent to
     // main activity first and then passed to the following method
     private val searchTextWatcher = object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
 
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            with(binding) {
-                symbolNotFoundPlaceholder.root.isVisible = false
-                typeToSearchPlaceholder.root.isVisible = false
-                searchSymbolProgressBar.isVisible = true
-                queryResultsRecyclerView.isInvisible = true
-            }
-        }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
 
         override fun afterTextChanged(s: Editable?) {
             val query = s.toString()
-            if (query.isNotEmpty()) {
-                viewModel.fetchSymbol(query)
-            } else {
-                binding.searchSymbolProgressBar.isVisible = false
-                binding.typeToSearchPlaceholder.root.visibility = View.VISIBLE
-            }
+            viewModel.fetchSymbol(query)
         }
     }
     private val onSearchResultClickListener = OnSearchResultClickListener { item ->
-        ApplicationPrefs
-            .Default(requireContext())
-            .saveChartSymbol(Symbol(item.symbol))
+        viewModel.saveSymbol()
         requireContext().hideKeyboard(view?.windowToken)
         findNavController().navigateUp()
+    }
+    private val tabOnSelectListener = object : TabLayout.OnTabSelectedListener {
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            tab?.let { tab ->
+                val filter = when (tab.position) {
+                    TAB_ALL -> SearchFilter.ALL
+                    TAB_STOCKS -> SearchFilter.STOCKS
+                    TAB_FX -> SearchFilter.FX
+                    TAB_INDEXES -> SearchFilter.INDEXES
+                    TAB_FUNDS -> SearchFilter.FUNDS
+                    TAB_FUTURES -> SearchFilter.FUTURES
+                    else -> throw IllegalStateException()
+                }
+                viewModel.updateFilter(filter)
+            }
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) = Unit
+
+        override fun onTabReselected(tab: TabLayout.Tab?) = Unit
     }
 
     override fun onCreateView(
@@ -81,14 +89,6 @@ class SearchSymbolFragment : Fragment(), OnSearchResultClickListener, VoiceQuery
         return binding.root
     }
 
-    override fun onSearchItemClick(item: SearchResultItem) {
-        ApplicationPrefs
-            .Default(requireContext())
-            .saveChartSymbol(Symbol(item.symbol))
-        requireContext().hideKeyboard(view?.windowToken)
-        findNavController().navigateUp()
-    }
-
     // Since the app reuses native Google voice recognition the voice query is sent to
     // main activity first and then passed to the following method
     override fun receiveVoiceQuery(query: String) {
@@ -98,38 +98,49 @@ class SearchSymbolFragment : Fragment(), OnSearchResultClickListener, VoiceQuery
 
     private fun setupViews() {
         with(binding) {
-            val searchAdapter = SearchResultAdapter(onSearchResultClickListener)
+            searchAdapter.listener = onSearchResultClickListener
+            searchCategoryTabLayout.addOnTabSelectedListener(tabOnSelectListener)
+
             queryResultsRecyclerView.apply {
                 this.adapter = searchAdapter
                 addItemDecoration(LineItemDecoration.Default(context))
             }
             symbolNotFoundPlaceholder.applyButton.setOnClickListener {
-                val query =
-                    (searchToolbar.menu.findItem(R.id.menu_search).actionView as SearchView).query
-                viewModel.fetchSymbol(query.toString())
+                viewModel.saveSymbol()
+                navigateBack()
             }
 
             viewModel.errorLiveData.observe(viewLifecycleOwner, {
-                searchSymbolProgressBar.isVisible = false
                 Toast.makeText(
                     requireContext(), R.string.warning_something_went_wrong, Toast.LENGTH_SHORT
                 ).show()
             })
             viewModel.resultLiveData.observe(viewLifecycleOwner, { list ->
-                searchSymbolProgressBar.isVisible = false
-                if (list.isNotEmpty()) {
-                    searchAdapter.list = list
-                    queryResultsRecyclerView.isVisible = true
-                } else {
-                    symbolNotFoundPlaceholder.root.isVisible = true
+                searchAdapter.items = list
+
+                if (list.isEmpty()) {
+                    viewModel.query.value?.let { query ->
+                        if (query.isNotEmpty()) {
+                            symbolNotFoundPlaceholder.root.isVisible = true
+                        } else {
+                            binding.typeToSearchPlaceholder.root.isVisible = true
+                        }
+                    }
                 }
             })
+            viewModel.isLoading.observe(viewLifecycleOwner) { value ->
+                searchSymbolProgressBar.isVisible = value
+
+                if (value) {
+                    symbolNotFoundPlaceholder.root.isVisible = false
+                    typeToSearchPlaceholder.root.isVisible = false
+                }
+            }
         }
 
         with(binding.searchToolbar) {
             setNavigationOnClickListener {
-                requireContext().hideKeyboard(view?.windowToken)
-                findNavController().navigateUp()
+                navigateBack()
             }
 
             (menu.findItem(R.id.menu_search).actionView as SearchView).apply {
@@ -145,10 +156,24 @@ class SearchSymbolFragment : Fragment(), OnSearchResultClickListener, VoiceQuery
                 findViewById<View>(appCompat.search_plate).background = null
                 findViewById<View>(appCompat.submit_area).background = null
                 findViewById<ImageView>(appCompat.search_mag_icon).apply {
-                    visibility = View.GONE
+                    isVisible = false
                     setImageDrawable(null)
                 }
             }
         }
+    }
+
+    private fun navigateBack() {
+        requireContext().hideKeyboard(view?.windowToken)
+        findNavController().navigateUp()
+    }
+
+    companion object {
+        private const val TAB_ALL = 0
+        private const val TAB_STOCKS = 1
+        private const val TAB_FX = 2
+        private const val TAB_INDEXES = 3
+        private const val TAB_FUNDS = 4
+        private const val TAB_FUTURES = 5
     }
 }
--- demo/src/main/java/com/chartiq/demo/ui/chart/searchsymbol/SearchSymbolViewModel.kt
+++ demo/src/main/java/com/chartiq/demo/ui/chart/searchsymbol/SearchSymbolViewModel.kt
@@ -3,38 +3,80 @@ package com.chartiq.demo.ui.chart.searchsymbol
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import com.chartiq.demo.ApplicationPrefs
 import com.chartiq.demo.network.NetworkManager
 import com.chartiq.demo.network.NetworkResult
 import com.chartiq.demo.network.model.SymbolResponse
 import com.chartiq.demo.ui.chart.searchsymbol.list.SearchResultItem
 import com.chartiq.demo.util.Event
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
-class SearchSymbolViewModel(private val networkManager: NetworkManager) : ViewModel() {
+class SearchSymbolViewModel(
+    private val networkManager: NetworkManager,
+    private val argPrefs: ApplicationPrefs
+) : ViewModel() {
+
+    private val searchJob = SupervisorJob()
+
+    private val searchScope = CoroutineScope(searchJob + Dispatchers.IO)
 
     val resultLiveData = MutableLiveData<List<SearchResultItem>>()
+
     val errorLiveData = MutableLiveData<Event<Unit>>()
 
+    val query = MutableLiveData("")
+
+    val filter = MutableLiveData(SearchFilter.ALL)
+
+    val isLoading = MutableLiveData(false)
+
     fun fetchSymbol(symbol: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            when (val result = networkManager.fetchSymbol(symbol)) {
+        isLoading.value = true
+        query.value = symbol
+        if (symbol.isEmpty()) {
+            resultLiveData.value = listOf()
+            searchJob.cancelChildren()
+            isLoading.value = false
+            return
+        }
+        searchScope.launch {
+            when (val result = networkManager.fetchSymbol(symbol, filter.value?.value)) {
                 is NetworkResult.Success -> resultLiveData.postValue(result.data.mapToItemList())
                 is NetworkResult.Failure -> errorLiveData.postValue(Event(Unit))
             }
+            isLoading.postValue(false)
         }
+    }
+
+    fun updateFilter(filter: SearchFilter) {
+        this.filter.value = filter
+        query.value?.let {
+            fetchSymbol(it)
+        }
+    }
+
+    fun saveSymbol() {
+        argPrefs.saveChartSymbol(Symbol(query.value!!))
     }
 
     private fun SymbolResponse.mapToItemList(): List<SearchResultItem> = payload.symbols
         .map { element -> element.split('|') }
         .map { SearchResultItem(it[0], it[1], it[2]) }
 
-    class SearchViewModelFactory(private val argNetworkManager: NetworkManager) :
-        ViewModelProvider.Factory {
+    class SearchViewModelFactory(
+        private val argNetworkManager: NetworkManager,
+        private val argPrefs: ApplicationPrefs
+    ) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return modelClass.getConstructor(NetworkManager::class.java)
-                .newInstance(argNetworkManager)
+            return modelClass
+                .getConstructor(
+                    NetworkManager::class.java,
+                    ApplicationPrefs::class.java
+                )
+                .newInstance(
+                    argNetworkManager,
+                    argPrefs
+                )
         }
     }
 }
