9994f29788e2256904deb6918fd8732abfc76526
Jacob <jacob@chartiq.com>
2022-10-26 12:41:45 -0400

    41779 mobile refresh interval
    Add script to access setRefreshInterval on the mobile bridge.

--- chartiq/src/main/java/com/chartiq/sdk/ChartIQ.kt
+++ chartiq/src/main/java/com/chartiq/sdk/ChartIQ.kt
@@ -108,6 +108,13 @@ interface ChartIQ : ChartIQDrawingTool, ChartIQStudy, ChartIQSignal {
     fun setChartType(chartType: ChartType)
 
     /**
+     * Sets ChartIQ refresh Interval.
+     * @param refreshInterval The ChartIQ refresh Interval in seconds.
+     */
+    fun setRefreshInterval(refreshInterval: Int)
+
+
+    /**
      * Gets a selected chart type
      * @param callback A callback to subscribe to to get a chart type
      */
